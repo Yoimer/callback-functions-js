4e04389ddc1fc7a314edf9fc0b3817a71bd72d66
Yoimer <ydrf19@gmail.com>
2018-05-27 00:45:09 -0400

    added advance example

--- script.js
+++ script.js
@@ -85,3 +85,43 @@ myArr.sort(function(val1, val2) {
 });
 
 console.log(myArr);
+
+//advanced example
+var students = [
+{name: "Mary", score: 90, school:"East"},
+{name: "James", score: 100, school:"East"},
+{name: "Steve", score: 40, school:"East"},
+{name: "Gabe", score: 90, school:"West"},
+{name: "Rachel", score: 85, school:"East"},
+{name: "Rochelle", score: 95, school:"West"},
+{name: "Lynette", score: 75, school:"East"}
+];
+
+var processStudents = function (data, callback) {
+    for(var i = 0; i < data.length; i++) {
+        if(data[i].school.toLowerCase() === "east") {
+            if (typeof callback === "function") {
+                callback(data[i]);
+            }
+        }
+    }
+}
+
+processStudents(students, function(obj) {
+    if (obj.score > 60) {
+        console.log(obj.name + " passed.");
+    }
+});
+
+// gets total and count them all
+determineTotal = function() {
+    var total = 0, count = 0;
+
+    processStudents(students, function(obj) {
+        total += obj.score;
+        count++;
+    });
+    console.log("Total score: " + total + " - Total Count:" + count);
+};
+
+determineTotal();
